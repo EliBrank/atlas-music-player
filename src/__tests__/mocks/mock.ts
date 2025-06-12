b6cdbcde346d1bd1f6c6db8e0297f6637f6f3b7c
eliBrank <ezb.business@pm.me>
2025-06-12 06:51:54 -0500

    configure mock server to mock calls to api

---
+++ src/__tests__/mocks/mock.ts
@@ -0,0 +1,75 @@
+import { setupServer } from "msw/node";
+import { http, HttpResponse } from "msw";
+
+type PlaylistSong = {
+  id: string,
+  title: string,
+  artist: string,
+  genre: string,
+  duration: number
+};
+
+type SongDetails = PlaylistSong & {
+  cover: string,
+  song: string,
+};
+
+const mockPlaylist: PlaylistSong[] = [
+  {
+    id: "abc001",
+    title: "Back to Testing",
+    artist: "The Testers",
+    genre: "Test Rock",
+    duration: 200,
+  },
+  {
+    id: "abc002",
+    title: "I Find Myself Testing",
+    artist: "The Testers",
+    genre: "Test Rock",
+    duration: 300
+  },
+];
+
+// Record is TS utility type for ensuring type of key/value
+const mockSongDetails: Record<string, SongDetails> = {
+  "abc001": {
+    id: "abc001",
+    title: "Back to Testing",
+    artist: "The Testers",
+    genre: "Test Rock",
+    duration: 200,
+    cover: "/placeholder.svg",
+    song: "https://example.com/songs/back-to-testing"
+  },
+  "abc002": {
+    id: "abc002",
+    title: "I Find Myself Testing",
+    artist: "The Testers",
+    genre: "Test Rock",
+    duration: 300,
+    cover: "/placeholder.svg",
+    song: "https://example.com/songs/i-find-myself-testing"
+  }
+}
+
+export const handlers = [
+  // mock playlist endpoint
+  http.get('api/vi/playlist', () => {
+    return HttpResponse.json(mockPlaylist);
+  }),
+
+  // mock song details endpoint
+  http.get('api/v1/songs/:songId', ({ params }) => {
+    const { songId } = params;
+    const song = mockSongDetails[songId as string];
+
+    if (!song) {
+      return new HttpResponse(null, { status: 404 });
+    }
+
+    return HttpResponse.json(song);
+  }),
+]
+
+export const server = setupServer(...handlers);
